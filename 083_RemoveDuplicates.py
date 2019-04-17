275f65682c1162650e262866b5a7c1f1ba5d220d
dionysus_pc <dionysus.cho@gmail.com>
2019-04-17 02:30:15 -0400

    Leetcode 083 - Init

---
+++ 083_RemoveDuplicates.py
@@ -0,0 +1,14 @@
+"""
+Given a sorted linked list, delete all duplicates such that each element appear
+only once.
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+
