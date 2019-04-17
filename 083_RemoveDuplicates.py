3d839bd888cf52286e4b52f19c772226ea1de1ad
dionysus_pc <dionysus.cho@gmail.com>
2019-04-17 15:36:04 -0400

    Leetcode 083 - Accepted

--- 083_RemoveDuplicates.py
+++ 083_RemoveDuplicates.py
@@ -4,11 +4,25 @@ only once.
 """
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+# class ListNode():
+#      def __init__(self, x):
+#          self.val = x
+#          self.next = None
 
-class Solution:
-    def deleteDuplicates(self, head: ListNode) -> ListNode:
 
+def deleteDuplicates(self, head: ListNode) -> ListNode:
+
+    if head.next is None:
+        return head
+
+    start = curr = head
+
+    while curr is not None:
+        if curr.next is None:
+            break
+        elif curr.val == curr.next.val:
+            curr.next = curr.next.next
+        elif curr.val != curr.next.val:
+            curr = curr.next
+
+    return start
