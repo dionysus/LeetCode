cc1a8eda179e947bde36f8eb984cfcd15dde699d
dionysus <dionysus.cho@gmail.com>
2019-04-25 01:30:54 -0400

    Leetcode 002 - Accepted

--- 002_AddTwoNumbers.py
+++ 002_AddTwoNumbers.py
@@ -22,38 +22,29 @@ def addTwoNumbers(l1, l2):
         return None
 
     else:
-
-        v01 = 0
-        v02 = 0
-
         if l1 and not l2:
-            v01 = l1.val
+            v = l1.val
         elif l2 and not l1:
-            v02 = l2.val
+            v = l2.val
             l1, l2 = l2, None
         else:
-            v01 = l1.val
-            v02 = l2.val
+            v = l1.val + l2.val
 
-        v = v01 + v02
         if v >= 10:
-            l1.val = v - 10
+            v -= 10
             if l1.next is None:
-                l1.next = ListNode(1)
+                if l2 is None or l2.next is None:
+                    l1.next = ListNode(1)
+                else:
+                    l2.next.val += 1
             else:
                 l1.next.val += 1
 
-        else:
-            l1.val = v
+        l1.val = v
 
-        if not l1 and not l2:
-            l1.next = None
-        elif l1 and not l2:
+        if not l2:
             l1.next = self.addTwoNumbers(l1.next, None)
-            return l1
-        elif l2 and not l1:
-            l2.next = self.addTwoNumbers(l2.next, None)
-            return l2
         else:
             l1.next = self.addTwoNumbers(l1.next, l2.next)
-            return l1
+            
+        return l1
